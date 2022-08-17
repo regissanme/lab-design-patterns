c2bfacbb3ba0b9e4b851415e2d4cc8cd6bf5eba2
regissanme <rsanme@gmail.com>
2022-08-16 23:14:02 -0300

    add Cliente controller

---
+++ gof/src/main/java/com/regissanme/gof/controller/ClienteController.java
@@ -0,0 +1,49 @@
+package com.regissanme.gof.controller;
+
+import com.regissanme.gof.model.Cliente;
+import com.regissanme.gof.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Projeto: gof
+ * Desenvolvedor: Reginaldo Santos de Medeiros (regissanme)
+ * Data: 16/08/2022
+ * Hora: 13:59
+ */
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+        clienteService.inserir(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        clienteService.atualizar(id, cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletar(@PathVariable Long id) {
+        clienteService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
